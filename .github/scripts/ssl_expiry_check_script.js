4ffff3a0598812dc5f161913733455703b1d1fd2
root <root@ip-172-31-42-83.ec2.internal>
2023-08-22 16:43:48 +0000

    Add SSL expiry check workflow and script

--- .github/scripts/ssl_expiry_check_script.js
+++ .github/scripts/ssl_expiry_check_script.js
@@ -5,6 +5,23 @@ const { exec } = require('child_process');
 
 const execPromise = promisify(exec);
 
+// Function to send Slack alert
+async function sendSlackAlert(domain, daysUntilExpiry) {
+  const slackToken = process.env.SLACK_WEBHOOK_URL;
+
+  const slackMessage = `SSL Expiry Alert\n   * Domain : ${domain}\n   * Warning : The SSL certificate for ${domain} will expire in ${daysUntilExpiry} days.`;
+
+  try {
+    await axios.post(slackToken, {
+      text: slackMessage,
+    });
+
+    console.log(`Slack alert sent for ${domain}`);
+  } catch (error) {
+    console.error(`Error sending Slack alert for ${domain}:`, error);
+  }
+}
+
 async function main() {
   try {
     const domains = await fs.readFile('domains.txt', 'utf-8');
@@ -20,7 +37,6 @@ async function main() {
     console.error(error);
   }
 }
-// Rest of the script (refer to previous responses for full script)
 
 main();
 
