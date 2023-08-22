11e2d77841bd22944336349ff51cafc42a159f37
root <root@ip-172-31-42-83.ec2.internal>
2023-08-22 21:56:48 +0000

    Add SSL expiry check workflow and script

--- .github/scripts/ssl_expiry_check_script.js
+++ .github/scripts/ssl_expiry_check_script.js
@@ -14,10 +14,26 @@ async function getSSLCertificateInfo(domain) {
     const currentDate = new Date();
     const daysUntilExpiry = Math.ceil((expirationDate - currentDate) / (1000 * 60 * 60 * 24));
 
-    return { domain, daysUntilExpiry };
+    return { daysUntilExpiry };
   }
 
-  return { domain, daysUntilExpiry: -1 };
+  return { daysUntilExpiry: -1 };
+}
+
+async function sendSlackAlert(domain, daysUntilExpiry) {
+  const slackToken = process.env.SLACK_WEBHOOK_URL;
+
+  const slackMessage = `SSL Expiry Alert\n   * Domain : ${domain}\n   * Warning : The SSL certificate for ${domain} will expire in ${daysUntilExpiry} days.`;
+
+  try {
+    await axios.post(slackToken, {
+      text: slackMessage,
+    });
+
+    console.log(`Slack alert sent for ${domain}`);
+  } catch (error) {
+    console.error(`Error sending Slack alert for ${domain}:`, error);
+  }
 }
 
 async function main() {
@@ -25,18 +41,21 @@ async function main() {
     const domains = await fs.readFile('.github/scripts/domains.txt', 'utf-8');
     const domainList = domains.split('\n').filter(domain => domain.trim() !== '');
 
-    const alerts = [];
-
     for (const domain of domainList) {
       const sslInfo = await getSSLCertificateInfo(domain);
-      alerts.push(sslInfo);
-    }
+      console.log(`Domain: ${domain}`);
+      console.log(`Days Until Expiry: ${sslInfo.daysUntilExpiry}`);
 
-    return alerts;
+      if (sslInfo.daysUntilExpiry <= 30) {
+        await sendSlackAlert(domain, sslInfo.daysUntilExpiry);
+      }
+    }
   } catch (error) {
     console.error(error);
   }
 }
 
-module.exports = main;
+main().catch(error => {
+  console.error('Unhandled promise rejection:', error);
+});
 
