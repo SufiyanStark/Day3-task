8a78323905db8d7e3f1beff03626411cc3b18882
root <root@ip-172-31-42-83.ec2.internal>
2023-08-22 21:45:13 +0000

    Add SSL expiry check workflow and script

--- .github/scripts/ssl_expiry_check_script.js
+++ .github/scripts/ssl_expiry_check_script.js
@@ -14,26 +14,10 @@ async function getSSLCertificateInfo(domain) {
     const currentDate = new Date();
     const daysUntilExpiry = Math.ceil((expirationDate - currentDate) / (1000 * 60 * 60 * 24));
 
-    return { daysUntilExpiry };
+    return { domain, daysUntilExpiry };
   }
 
-  return { daysUntilExpiry: -1 };
-}
-
-async function sendSlackAlert(domain, daysUntilExpiry) {
-  const slackToken = process.env.SLACK_WEBHOOK_URL;
-
-  const slackMessage = `SSL Expiry Alert\n   * Domain : ${domain}\n   * Warning : The SSL certificate for ${domain} will expire in ${daysUntilExpiry} days.`;
-
-  try {
-    await axios.post(slackToken, {
-      text: slackMessage,
-    });
-
-    console.log(`Slack alert sent for ${domain}`);
-  } catch (error) {
-    console.error(`Error sending Slack alert for ${domain}:`, error);
-  }
+  return { domain, daysUntilExpiry: -1 };
 }
 
 async function main() {
@@ -41,27 +25,18 @@ async function main() {
     const domains = await fs.readFile('.github/scripts/domains.txt', 'utf-8');
     const domainList = domains.split('\n').filter(domain => domain.trim() !== '');
 
-    let alertMessage = '';
+    const alerts = [];
 
     for (const domain of domainList) {
       const sslInfo = await getSSLCertificateInfo(domain);
-      const domainMessage = `Domain: ${domain}\nDays Until Expiry: ${sslInfo.daysUntilExpiry}`;
-      console.log(domainMessage);
-      alertMessage += domainMessage + '\n\n';
-      if (sslInfo.daysUntilExpiry <= 30) {
-        await sendSlackAlert(domain, sslInfo.daysUntilExpiry);
-      }
+      alerts.push(sslInfo);
     }
 
-    return alertMessage.trim();
+    return alerts;
   } catch (error) {
     console.error(error);
   }
 }
 
-main().then(alertMessage => {
-  console.log(alertMessage);
-}).catch(error => {
-  console.error('Unhandled promise rejection:', error);
-});
+module.exports = main;
 
